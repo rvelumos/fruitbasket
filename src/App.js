a1d7731c957a07e4340786522499d644c4b0a4cc
Ronald <ronald.eijsden@gmail.com>
2021-03-19 16:01:08 +0100

    Orderbasket + Orderform added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import OrderBasket from "./Components/OrderBasket/OrderBasket";
+import OrderForm from "./Components/OrderForm/OrderForm";
 
 function App() {
+
   return (
     <>
       <h1>Fruitmand bezorgservice</h1>
+      <OrderBasket />
+      <OrderForm />
     </>
   );
 }
